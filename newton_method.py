f559d3d606a27bd6203d2a3bf14aa085cc0a6738
dhwang99 <dhwang99@163.com>
2017-06-19 19:31:15 +0800

    add unconstrained optimized

--- newton_method.py
+++ newton_method.py
@@ -4,8 +4,9 @@ import numpy as np
 import pdb
 
 '''
-因为牛顿法、拟牛顿法在非线性优化上用得比较广，单独写一个文件
-包括牛顿法、
+牛顿法、拟牛顿法用在非线性优化上
+不过因为牛顿法、拟牛顿法在非线性优化上用得比较广，单独写一个文件
+包括:
 '''
 
 def f4():
@@ -19,8 +20,27 @@ def f4():
     return c,b,A
 
 '''
-牛顿法. 要求有二阶导数
-非区间搜索法。只要给到起始点，就可以下降(对凸函数是这样子)
+牛顿法. 要求f有二阶导数
+牛顿法非区间搜索法。只要给到起始点，就可以下降(对凸函数是这样子)
+
+算法大致如下：
+对原函数二阶tayler展开，然后用二次多项式近似：
+f(x) = f(x0) + f'(x)(x - x0) + 1/2(x-x0).T * H * (x - x0) + O((x-x0).T(x-x0))
+f_sim(x) = f(x0) + f'(x)(x - x0) + 1/2(x-x0).T * H * (x - x0)
+f_sim'(x) = f'(x) + H(x-x0) = 0
+x = x0 - H.inv * f'(x)
+
+dk = -H.inv * f', 称为牛顿方向
+
+牛顿法求解，计算量比较大，还要求H正定
+H为海森矩阵, 要求是非奇异的
+
+阻尼牛顿法
+ 由于牛顿法并不能保证收敛.于是改为沿dk方向进行一维搜索求，xk+1 = xk + lambk * dk, 
+
+实际上并不直接求H.inv, 而改为解线性方程：
+H*dk = f'
+
 '''
 def newton_search_for_quad(f, x0, espilon):
     c,b,A = f()
@@ -44,6 +64,38 @@ def newton_search_for_quad(f, x0, espilon):
 
     return None
 
+'''
+拟牛顿条件
+如上式：
+~=: sim_eq
+f(x)在x_k+1点处理展开
+f(x) ~= f(x_k1) + f'(x_k1)(x-xk) + 1/2*(x_k1-x).T * H * (x_k1-x)
+两边同时作用一个梯度算子，则：
+f'(x) ~= f'(x_k1) + H(x_k1)*(x_k1 - x)
+记 x=x_k
+   f'(x_k) ~= f'(x_k1) + H(x_k1) * (x_k1 - x_k)
+   f'(x_k1) - f'(x_k) ~= H(x_k1) * (x_k1 - x_k)
+
+记 B=H, D=H.inv, y_k = f'(x_k1) - f'(x_k), s_k = (x_k1 - x_k),则
+y_k ~= H*s_k 
+s_k ~= H.inv * y_k
+上述即为拟牛顿条件，
+对 H(k+1)或H(k+1).inv做近似  
+y_k = B(k+1)*s_k
+或：
+s_k = D(k+1) * y_k
+
+即用梯度近似计算H或H的逆
+'''
+
+def DFP(f, f_deriv, x0, espilon):
+    return None
+
+def BFGS(f, f_deriv, x0, espilon):
+    return None
+
+def L_BFGS(f, f_deriv, x0, espilon):
+    return None
 
 if __name__ == "__main__":
     x0 = np.matrix('0.0;0.0') 
--- uncostrained_optimize.py
+++ uncostrained_optimize.py
@@ -23,6 +23,9 @@ import pdb
                        = xp'AAxp + kxp'AAAxp 
 
 3. 共轭梯度方向下降法
+  共轭方向法，开始用来求解 Ax=b方程组. 而在求解二次函数最优解时，其梯度函数为  Ax+b=0的解, 
+  于是也用这个方法来求解二次函数的最优解。其它非二次的凸函数，则可以用二次函数来近似
+
   (x, Ay) = 0, 称x,y为共轭向量 (正交是共轭的特殊形式，A为单位阵)
   3.1 找到n个共轭向量 ui
   3.2 沿共轭向量方向进行最优搜索, 得到每次搜索的最优步长列表 lbi
@@ -30,17 +33,13 @@ import pdb
        x_star = x0 + sum(lbi * ui)
 
   对非二次函数，可以这样逼近最优解
-
-
 '''
-
-
 
 def f_value(f, x):
     c,b,A = f()
     val = c + b.T * x + 1./2.*x.T * A * x
 
-    return val
+    return np.sum(val)
 
 '''
 f = 1/2(x1 * x1 + 2x2 * x2)
@@ -108,7 +107,7 @@ def f2():
 from line_search import golden_section_search 
 
 '''
-共轭法：
+共轭方向法：
 1. 使用黄金搜索找lambda
 '''
 def conj_grandient_method_for_f2():
@@ -122,28 +121,18 @@ def conj_grandient_method_for_f2():
     
     '''
     线性搜索用的一次函数, 参数为k
-    f = f(x0 + kf'(x0))
+    f = f(xi + kui)
     '''
-    def k_f1(k):
-        kx = x0 + k * u1 
-        fk = f_value(f2, kx) 
-        return np.sum(fk)
-   
-    k1 = golden_section_search(k_f1, def_field, esplison)
+    k1 = golden_section_search(lambda k:f_value(f2, x0 + k*u1), def_field, esplison)
     x1 = x0 + k1[0] * x0
 
-    def k_f2(k):
-        kx = x1 + k * u2 
-        fk = f_value(f2, kx) 
-        return np.sum(fk)
-
-    k2 = golden_section_search(k_f2, def_field, esplison)
+    k2 = golden_section_search(lambda k:f_value(f2, x1 + k*u2), def_field, esplison)
     x2 = x0 + k1[0] * u1 + k2[0] * u2 
     
     return x2, f_value(f2, x2)
 
 '''
-共轭法：
+共轭方向法：
 2. 对二次函数直接求解lambda
 lamb_i+1 = (u(i+1), Ax0 + b) / (u(i+1), Au(u+1))
 '''
@@ -161,12 +150,131 @@ def conj_grandient_method_for_f2_direct():
     return x2, f_value(f2, x2)
 
 
+def conj_f3():
+    A = np.matrix('1,1;1,2')
+    c = 0
+    b = np.matrix('0;0')
+
+    return c,b,A
+
+'''
+Fletcher_Reeves_conj
+关于v0,v1,...vn共轭，最好推导一次
+x0, x1, ...
+v0, v1,....
+xi = xi_1 + lambda * vi_1
+vi = -gi + ||gi||/||gi_1|| * vi_1  
+沿共轭方向求极小值：
+gi, 第xi点的梯度值
+
+example:
+
+f=1/2x.TAx
+A=[1,1;1,2]
+x0 = [10.;-5.]
+v0 = g0
+
+lamb0 = 0.75
+x1 = [1.25,-3.75]
+v1=[-4.36,3.75]
+lamb1 = 1.34
+
+x2 = [0.4, 0.01]
+再迭代一次?
+讲义上似乎算错了。有空算一下
+'''
+def Fletcher_Reeves_conj():
+    f = conj_f3
+    c,b,A = f()
+    x0 = np.matrix('10.;-5.')
+    g0 = A * x0 + b 
+    v0 = -g0
+
+    #pdb.set_trace() 
+    lamb0, f_x0 = golden_section_search(lambda k:f_value(f, x0 + k*v0), [0,2], 0.001) 
+
+    x1 = x0 + lamb0 * v0
+    g1 = A*x1 + b 
+    v1 = -g1 + np.dot(g1.T, g1)[0,0]/np.dot(g0.T, g0)[0,0] * v0
+    lamb1, f_x1 = golden_section_search(lambda k:f_value(f, x1 + k*v1), [0,2], 0.001) 
+    x2 = x1 + lamb1 * v1
+    return x2, f_x1 
+
+'''
+'''
+def f_powell():
+    c = 0
+    b = np.matrix('0.;0.')
+    A = np.matrix('2,0;0,4')
+
+    return c,b,A
+
+'''
+相比于fletcher算法，powell算不需要计算梯度。但需要有n个线性无关的初始向量, 
+
+1. 每一步的过程 
+   xi = xi_1 + lambda * vi_1, lambda线性搜索后的最小值
+   vi --> vi_1
+   xn-x0 --> vn
+   u0 = xn-x0
+   x0 = xn + lambda * (xn - x0), 为新的x0值
+
+   算法本身所需要的步数，并不比fletcher小
+
+2. 重复上述步骤，直到收敛 
+  留意下收敛条件：如 ||xi - xi_1|| < espilon, |fi - fi_1| < esplilon, max_steps
+
+
+这种产生共轭向量的方法，并没有推导，只有实现。最好去推一下
+
+
+这两个算法，和DFP/BFGS关系密切。具体参考 newton算法相关内容
+
+'''
+def powell_conj():
+    '''
+    u1=[-11.14, -24.46]
+    u2=[-1.8, -0.28]
+    '''
+    x0 = np.matrix('20.;20.')
+    #v1,v2线性无关
+    v = np.matrix('1.,1.;-1.,1.')
+
+    c,b,A = f_powell()
+    u = np.matrix('0.,0.;0.,0.')
+    lamb = np.matrix('0.;0.')
+   
+    id = 0
+    total = 0
+    while total < 3:
+        k,min_fk = golden_section_search(lambda k:f_value(f_powell, x0 + k*v[:,0]), [-100., 100], 0.001)
+        x1 = x0 + k * v[:,0]
+
+        k,min_fk = golden_section_search(lambda k:f_value(f_powell, x1 + k*v[:,1]), [-100., 100], 0.001)
+        x2 = x1 + k * v[:,1]
+
+        #找到u向量
+        u[:,id] = x2 - x0
+        k,min_fk = golden_section_search(lambda k:f_value(f_powell, x2 + k*u[:, id]), [-100., 100], 0.001)
+
+        x0 = x2 + k*u[:,id]
+        v[:,0] = v[:,1]
+        v[:,1] = u[:,id]
+
+        id = (id+1) % len(lamb)
+        total += 1
+
+        conj = u[:,0].T * A * u[:,1]
+        #print "conj: ", conj
+
+    x_star = x0
+
+    return x_star, f_value(f_powell, x_star)
+
 from line_search import newton_search_for_quad
 from newton_method import newton_search_for_quad 
 
 if __name__ == "__main__":
-    print "test"
-    
     rs = solve_direct(f1)
     print "\nsolve_direct:", rs
 
@@ -189,3 +297,9 @@ if __name__ == "__main__":
 
     rs = solve_direct(f2)
     print "\nsolve_direct:", rs
+
+    frs = Fletcher_Reeves_conj()
+    print "Fletcher_Reeves_conj.\nexpect: x2 = [0.4, 0.01]. \nReal:", frs
+
+    frs = powell_conj()
+    print "Fletcher_Reeves_conj.\nexpect: x2 = [0.4, 0.01]. \nReal:", frs
